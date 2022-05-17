e2ec0d090d6a471364b4a69e33e1dd7ca6ba4d5f
Medhekar <Bhavesh.Medhekar@saint-gobain.com>
2022-05-17 11:26:19 +0530

    theme options adde

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,9 +1,22 @@
 var FIproject = {};
 var winWidth = $(window).width();
 
-
+function change_theme() {
+    $(".comp-theme-selector .item").click(function() {
+        var selectedTheme = $(this).data("theme");
+        var themeClasses = ['theme-1', 'theme-2', 'theme-3', 'theme-4'];
+        var $body = $('body');
+        $.each(themeClasses, function(i, v) {
+            $body.removeClass(v);
+        });
+        localStorage.setItem("theme", selectedTheme); //setting theme to local storage
+        $("body").addClass(selectedTheme);
+    })
+}
 
 $(function() {
-    /* document ready*/
+    change_theme();
+    var theme = localStorage.getItem('theme'); //getting back theme from localstorage  
+    $("body").addClass(theme); //adding class to body tag
 
 });
